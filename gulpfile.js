10600dd545395d22fcf279b30912d912124a700e
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2014-10-11 18:49:05 +0900

    Remove unused variable.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,6 @@
 var gulp = require('gulp');
 var karma = require('karma').server;
 
-var paths = {
-  elements: 'elements/**',
-};
-
 gulp.task('test', function(done) {
   karma.start({
     configFile: __dirname + '/karma.conf.js',
